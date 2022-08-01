cc83735ba644cf0c9bcd49e4c5ccd3bdfc22d0d2
Abdu_ <abderraouf.dev@gmail.com>
2022-08-01 22:22:02 +0100

    make it better

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const authRoute = require("./routes/Auth");
 const userRoute = require("./routes/Users");
 const postRoute = require("./routes/Posts");
 const categoryRoute = require("./routes/Categories");
-const multer = require("multer");
-const path = require("path");
 
 dotenv.config();
 app.use(express.json());
@@ -16,26 +14,6 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
-
-  //STORAGE FOR PROFILE PICs
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, "troll51.jpg");
-  },
-});
-
-
-//Upload end point for PROFILE PICs
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-    console.log(req.body)
-  res.status(200).json("File has been uploaded");
-});
-
-
 
 
 
